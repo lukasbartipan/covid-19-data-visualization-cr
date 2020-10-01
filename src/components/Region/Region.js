4879216d5bda3fe085e697e446e5494ec73d687e
LukasBartipan <lukasbartipan@gmail.com>
2020-10-02 00:18:23 +0200

    region data display fix

--- src/components/Region/Region.js
+++ src/components/Region/Region.js
@@ -2,22 +2,23 @@ import React from 'react';
 
 import styles from "./Region.module.css";
 
-const Region = ({regionData, mousePos}) => {
-    if (!regionData?.datum)
+const Region = ({regionData: {data, name}, mousePos}) => {
+    if (!data?.datum)
         return ''
 
     return (
-        <div className={styles.region_container} style={{left: mousePos.X, top: mousePos.Y + 175, pointerEvents: "none"}}>
+        <div className={styles.region_container} style={{left: mousePos.X + 'px', top: mousePos.Y + 'px', pointerEvents: "none"}}>
+            <h4>{name}</h4>
             <div>
-                Nakažení: {regionData.pocet_nakazenych}
+                Nakažení: {data.pocet_nakazenych}
             </div>
             <div>
-                Vyléčení: {regionData.pocet_vylecenych}
+                Vyléčení: {data.pocet_vylecenych}
             </div>
             <div>
-                Umrtí: {regionData.pocet_umrti}
+                Umrtí: {data.pocet_umrti}
             </div>
-            <p className={styles.date}>Aktuální k: {new Date(regionData.datum).toLocaleDateString('cs-CZ')}</p>
+            <p className={styles.date}>Aktuální k: {new Date(data.datum).toLocaleDateString('cs-CZ')}</p>
         </div>
     )
 }
